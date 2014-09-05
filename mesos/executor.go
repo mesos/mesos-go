11b5a58e29342509d9b2a8e8c619acc1318fcd03
Kamil Domanski <kdomanski@kdemail.net>
2014-09-05 03:16:47 +0200

    remove C++11 CGO flags
    C++11 is not needed in the bindings and the flag prohibits build with older GCC

--- mesos/executor.go
+++ mesos/executor.go
@@ -4,7 +4,6 @@ package mesos
 #cgo LDFLAGS: -L. -L/usr/local/lib -lmesos
 #cgo linux LDFLAGS: -lstdc++
 #cgo darwin CXXFLAGS: -stdlib=libc++
-#cgo CXXFLAGS: -std=c++11
 #cgo CFLAGS:-I. -I/usr/local/include -I/usr/local/include/mesos
 
 #include <string.h>

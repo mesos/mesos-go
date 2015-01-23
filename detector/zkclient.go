bbfe594f2d9474c6eae24ce7ad199768b6285c07
vladimirvivien <vladimir.vivien@gmail.com>
2015-01-23 15:00:11 -0500

    removed the zookeepr package, keep detector impl specific.

--- detector/zkclient.go
+++ detector/zkclient.go
@@ -5,16 +5,44 @@ import (
 	"fmt"
 	log "github.com/golang/glog"
 	"github.com/samuel/go-zookeeper/zk"
+	"sort"
 	"time"
 )
 
+// zkChildrenWatcher interface for handling watcher event
+// when zk.EventNodeChildrenChanged.
+type zkChildrenWatcher interface {
+	childrenChanged(*zkClient, string)
+}
+
+// zkChildrenWatcherFunk adapter function type to facade the interface.
+type zkChildrenWatcherFunc func(*zkClient, string)
+
+func (fn zkChildrenWatcherFunc) childrenChanged(zkc *zkClient, path string) {
+	fn(zkc, path)
+}
+
+// zkErrorWatcher interface for handling errors.
+type zkErrorWatcher interface {
+	errorOccured(*zkClient, error)
+}
+
+// zkErrorWatcherFunc adapter function to facade zkErrorWatcher.
+type zkErrorWatcherFunc func(*zkClient, error)
+
+func (fn zkErrorWatcherFunc) errorOccured(zkc *zkClient, err error) {
+	fn(zkc, err)
+}
+
 type zkClient struct {
-	conn        zkConnector
-	hosts       []string
-	connTimeout time.Duration
-	connected   bool
-	stopCh      chan bool
-	rootPath    string
+	conn            zkConnector
+	hosts           []string
+	connTimeout     time.Duration
+	connected       bool
+	stopCh          chan bool
+	rootPath        string
+	childrenWatcher zkChildrenWatcher
+	errorWatcher    zkErrorWatcher
 }
 
 func newZkClient(hosts []string, path string) (*zkClient, error) {
@@ -47,10 +75,9 @@ func (zkc *zkClient) connect() error {
 			case e := <-ch:
 				if e.Err != nil {
 					log.Errorf("Received state error: %s", e.Err.Error())
-					// if c.watcher != nil {
-					// 	go c.watcher.Error(e.Err)
-					// }
-					//zkc.disconnect()
+					if zkc.errorWatcher != nil {
+						zkc.errorWatcher.errorOccured(zkc, e.Err)
+					}
 				}
 				switch e.State {
 				case zk.StateConnecting:
@@ -60,10 +87,6 @@ func (zkc *zkClient) connect() error {
 					zkc.connected = true
 					log.Infoln("Connected to zookeeper at", zkc.hosts)
 					close(waitConnCh)
-
-					// if c.watcher != nil {
-					// 	go c.watcher.Connected(c)
-					// }
 
 				case zk.StateSyncConnected:
 					zkc.connected = true
@@ -98,6 +121,73 @@ func (zkc *zkClient) disconnect() error {
 	return nil
 }
 
-func (zkc *zkClient) watch(path string) error {
+func (zkc *zkClient) watchChildren(path string) error {
+	if !zkc.connected {
+		return errors.New("Not connected to server.")
+	}
+	watchPath := zkc.rootPath
+	if path != "" && path != "." {
+		watchPath = watchPath + path
+	}
+
+	log.V(2).Infoln("Watching children for path", watchPath)
+	children, _, ch, err := zkc.conn.ChildrenW(watchPath)
+	if err != nil {
+		return err
+	}
+
+	go func(chList []string) {
+		select {
+		case e := <-ch:
+			if e.Err != nil {
+				log.Errorf("Received error while watching path %s: %s", watchPath, e.Err.Error())
+				if zkc.errorWatcher != nil {
+					zkc.errorWatcher.errorOccured(zkc, e.Err)
+				}
+			}
+
+			switch e.Type {
+			case zk.EventNodeChildrenChanged:
+				if zkc.childrenWatcher != nil {
+					zkc.childrenWatcher.childrenChanged(zkc, e.Path)
+				}
+			}
+		}
+		err := zkc.watchChildren(path)
+		if err != nil {
+			log.Errorf("Unable to watch children for path %s: %s", path, err.Error())
+			if zkc.errorWatcher != nil {
+				zkc.errorWatcher.errorOccured(zkc, err)
+			}
+		}
+	}(children)
 	return nil
+}
+
+func (zkc *zkClient) list(path string) ([]string, error) {
+	if !zkc.connected {
+		return nil, errors.New("Unable to list children, client not connected.")
+	}
+
+	children, _, err := zkc.conn.Children(path)
+	if err != nil {
+		return nil, err
+	}
+
+	// sort children (ascending).
+	sort.Strings(children)
+	return children, nil
+}
+
+func (zkc *zkClient) data(path string) ([]byte, error) {
+	if !zkc.connected {
+		return nil, errors.New("Unable to retrieve node data, client not connected.")
+	}
+
+	data, _, err := zkc.conn.Get(path)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
 }
--- detector/zkclient_test.go
+++ detector/zkclient_test.go
@@ -1,13 +1,14 @@
 package detector
 
 import (
-	"strings"
-	// log "github.com/golang/glog"
+	"errors"
+	log "github.com/golang/glog"
 	"github.com/samuel/go-zookeeper/zk"
 	"github.com/stretchr/testify/assert"
-	"testing"
-	// "time"
 	"os"
+	"strings"
+	"testing"
+	"time"
 )
 
 var test_zk_hosts = []string{"localhost:2181"}
@@ -25,7 +26,8 @@ func TestZkClientNew(t *testing.T) {
 
 }
 
-// This test requires zookeeper to be running and env ZK_HOSTS set.
+// This test requires zookeeper to be running.
+// You must also set env variable ZK_HOSTS to point to zk hosts.
 // The zk package does not offer a way to mock its connection function.
 func TestZkClientConnect(t *testing.T) {
 	if os.Getenv("ZK_HOSTS") == "" {
@@ -40,6 +42,75 @@ func TestZkClientConnect(t *testing.T) {
 	err = c.connect()
 	assert.NoError(t, err)
 	assert.True(t, c.connected)
+}
+
+func TestWatchChildren(t *testing.T) {
+	path := "/test"
+	ch := make(chan zk.Event, 1)
+	e := zk.Event{
+		Type: zk.EventNodeChildrenChanged,
+		Path: path,
+	}
+
+	c := makeZkClient(t, test_zk_hosts, path)
+	c.conn = makeMockConnector(path, (<-chan zk.Event)(ch))
+	wCh := make(chan struct{}, 1)
+	c.childrenWatcher = zkChildrenWatcherFunc(func(zkc *zkClient, path string) {
+		log.V(4).Infoln("Path", path, "changed!")
+		children, err := c.list(path)
+		assert.NoError(t, err)
+		assert.Equal(t, 3, len(children))
+		// assert sorted children
+		assert.Equal(t, "a", children[0])
+		assert.Equal(t, "d", children[1])
+		assert.Equal(t, "x", children[2])
+		wCh <- struct{}{}
+	})
+
+	err := c.watchChildren(".")
+	assert.NoError(t, err)
+
+	ch <- e // signal nodechildrenchanged event.
+
+	select {
+	case <-wCh:
+	case <-time.After(time.Millisecond * 700):
+		panic("Waited too long...")
+	}
+}
+
+func TestWatchErrors(t *testing.T) {
+	path := "/test"
+	ch := make(chan zk.Event, 1)
+	e := zk.Event{
+		Type: zk.EventNodeChildrenChanged,
+		Path: "/test",
+		Err:  errors.New("Event Error"),
+	}
+
+	c := makeZkClient(t, test_zk_hosts, path)
+	c.conn = makeMockConnector(path, (<-chan zk.Event)(ch))
+	wCh := make(chan struct{}, 1)
+	c.errorWatcher = zkErrorWatcherFunc(func(zkc *zkClient, err error) {
+		wCh <- struct{}{}
+	})
+
+	c.watchChildren(".")
+	ch <- e // signal nodechildrenchanged event.
+
+	select {
+	case <-wCh:
+	case <-time.After(time.Millisecond * 700):
+		panic("Waited too long...")
+	}
+
+}
+
+func makeZkClient(t *testing.T, hosts []string, path string) *zkClient {
+	c, err := newZkClient(hosts, path)
+	assert.NoError(t, err)
+	c.connected = true
+	return c
 }
 
 func makeMockConnector(path string, chEvent <-chan zk.Event) *MockZkConnector {

59834f803f2007cfacf3efc5cbad631765d13e52
James DeFelice <james.defelice@gmail.com>
2015-02-07 00:36:24 +0000

    started implementing detector builder, work-in-progress- will not compile

--- detector/standalone.go
+++ detector/standalone.go
@@ -6,18 +6,20 @@ import (
 )
 
 type Standalone struct {
+	current *mesos.MasterInfo
 	ch chan *mesos.MasterInfo
 }
 
 // Create a new stand alone master detector.
-func NewStandalone() *Standalone {
-	return &Standalone{make(chan *mesos.MasterInfo)}
+func NewStandalone(mi *mesos.MasterInfo) *Standalone {
+	return &Standalone{current: mi, ch: make(chan *mesos.MasterInfo)}
 }
 
 // Trigger a master detected event.
 func (s *Standalone) Appoint(m *mesos.MasterInfo) {
 	log.V(2).Infoln("Appoint")
 	s.ch <- m
+	s.current = m
 }
 
 // Detecting the new master.

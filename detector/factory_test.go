ba70a197afb2137c2abb44b2b42964cacde86766
Vladimir Vivien <vladimir.vivien@gmail.com>
2015-02-13 11:37:19 -0500

    Swipe at testing the detector.New()

---
+++ detector/factory_test.go
@@ -0,0 +1,36 @@
+package detector
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type testDetector string
+
+func (d testDetector) Detect(f MasterChanged) error { return nil }
+
+func (d testDetector) Done() <-chan struct{} { return make(<-chan struct{}) }
+
+func TestDetectorFactoryRegister(t *testing.T) {
+	Register("bbm:", func(spec string) (Master, error) {
+		return testDetector("Hello!"), nil
+	})
+	f, ok := plugins["bbm:"]
+	assert.True(t, ok)
+	assert.NotNil(t, f)
+}
+
+func TestDectorFactoryNew_EmptySpec(t *testing.T) {
+	m, err := New("")
+	assert.NoError(t, err)
+	sa, ok := m.(*Standalone)
+	assert.True(t, ok)
+}
+
+// TODO figure out how to test without cyclic import
+// func TestDectorFactoryNew_ZkPrefix(t *testing.T) {
+// 	m, err := New("zk://127.0.0.1:5050/mesos")
+// 	assert.NoError(t, err)
+// 	sa, ok := m.(zoo.MasterDetector)
+// 	assert.True(t, ok)
+// }

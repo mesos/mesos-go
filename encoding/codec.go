d78966d8f9713d1b749f9a588bfbbc9729eeae4f
Tomás Senart <tomas@mesosphere.io>
2015-10-21 09:05:10 +0100

    Abstract HTTP client

--- encoding/codec.go
+++ encoding/codec.go
@@ -5,6 +5,8 @@ import (
 	"io"
 
 	"github.com/mesos/mesos-go/encoding/proto"
+
+	pb "github.com/gogo/protobuf/proto"
 )
 
 const (
@@ -40,28 +42,52 @@ type (
 		// NewDecoder returns a new decoder for the defined media type.
 		NewDecoder func(io.Reader) Decoder
 	}
-	// An Encoder encodes a given object or returns an error in case of failure.
-	Encoder interface {
-		Encode(interface{}) error
+	// Marshaler composes the supported marshaling formats.
+	Marshaler interface {
+		pb.Marshaler
+		json.Marshaler
 	}
-	// A Decoder decodes a given object or returns an error in case of failure.
-	Decoder interface {
-		Decode(interface{}) error
+	// Unmarshaler composes the supporter unmarshaling formats.
+	Unmarshaler interface {
+		pb.Unmarshaler
+		json.Unmarshaler
 	}
+	// An Encoder encodes a given Marshaler or returns an error in case of failure.
+	Encoder func(Marshaler) error
+	// A Decoder decodes a given Unmarshaler or returns an error in case of failure.
+	Decoder func(Unmarshaler) error
 )
+
+// Encode is an utility method that calls the Encoder itself.
+func (e Encoder) Encode(m Marshaler) error { return e(m) }
+
+// Decode is an utility method that calls the Decoder itself.
+func (d Decoder) Decode(u Unmarshaler) error { return d(u) }
 
 // NewProtobufEncoder returns a new Encoder of Calls to Protobuf messages written to
 // the given io.Writer.
-func NewProtobufEncoder(w io.Writer) Encoder { return proto.NewEncoder(w) }
+func NewProtobufEncoder(w io.Writer) Encoder {
+	enc := proto.NewEncoder(w)
+	return func(m Marshaler) error { return enc.Encode(m) }
+}
 
 // NewJSONEncoder returns a new Encoder of Calls to JSON messages written to
 // the given io.Writer.
-func NewJSONEncoder(w io.Writer) Encoder { return json.NewEncoder(w) }
+func NewJSONEncoder(w io.Writer) Encoder {
+	enc := json.NewEncoder(w)
+	return func(m Marshaler) error { return enc.Encode(m) }
+}
 
 // NewProtobufDecoder returns a new Decoder of Protobuf messages read from the
 // given io.Reader to Events.
-func NewProtobufDecoder(r io.Reader) Decoder { return proto.NewDecoder(r) }
+func NewProtobufDecoder(r io.Reader) Decoder {
+	dec := proto.NewDecoder(r)
+	return func(u Unmarshaler) error { return dec.Decode(u) }
+}
 
 // NewJSONDecoder returns a new Decoder of JSON messages read from the
 // given io.Reader to Events.
-func NewJSONDecoder(r io.Reader) Decoder { return json.NewDecoder(r) }
+func NewJSONDecoder(r io.Reader) Decoder {
+	dec := json.NewDecoder(r)
+	return func(u Unmarshaler) error { return dec.Decode(u) }
+}

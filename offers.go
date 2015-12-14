477413742b66f01a8df9d3e0758a49aec05e3bc1
James DeFelice <james@mesosphere.io>
2015-12-14 08:26:29 +0000

    expose more intelligible error API

--- offers.go
+++ offers.go
@@ -7,18 +7,65 @@ import (
 	"github.com/gogo/protobuf/proto"
 )
 
-type offerResourceOp func(Offer_Operation, Resources) (Resources, error)
+type (
+	OperationErrorType int
+
+	OperationError struct {
+		errorType OperationErrorType
+		opType    Offer_Operation_Type
+		cause     error
+	}
+
+	offerResourceOp func(Offer_Operation, Resources) (Resources, error)
+	opErrorHandler  func(cause error) error
+)
+
+const (
+	OperationErrorTypeInvalid OperationErrorType = iota
+	OperationErrorTypeUnknown
+)
 
 var (
 	opNoop             = func(_ Offer_Operation, _ Resources) (_ Resources, _ error) { return }
 	offerResourceOpMap = map[Offer_Operation_Type]offerResourceOp{
 		LAUNCH:    opNoop,
-		RESERVE:   validateOpTotals(opReserve),
-		UNRESERVE: validateOpTotals(opUnreserve),
-		CREATE:    validateOpTotals(opCreate),
-		DESTROY:   validateOpTotals(opDestroy),
+		RESERVE:   handleOpErrors(invalidOp(RESERVE), validateOpTotals(opReserve)),
+		UNRESERVE: handleOpErrors(invalidOp(UNRESERVE), validateOpTotals(opUnreserve)),
+		CREATE:    handleOpErrors(invalidOp(CREATE), validateOpTotals(opCreate)),
+		DESTROY:   handleOpErrors(invalidOp(DESTROY), validateOpTotals(opDestroy)),
 	}
 )
+
+func (err *OperationError) Cause() error                    { return err.cause }
+func (err *OperationError) Type() OperationErrorType        { return err.errorType }
+func (err *OperationError) Operation() Offer_Operation_Type { return err.opType }
+
+func (err *OperationError) Error() string {
+	switch err.errorType {
+	case OperationErrorTypeInvalid:
+		return fmt.Sprintf("invalid "+err.opType.String()+" operation: %+v", err.cause)
+	case OperationErrorTypeUnknown:
+		return err.cause.Error()
+	default:
+		return fmt.Sprintf("operation error: %+v", err.cause)
+	}
+}
+
+func invalidOp(t Offer_Operation_Type) opErrorHandler {
+	return func(cause error) error {
+		return &OperationError{errorType: OperationErrorTypeInvalid, opType: t, cause: cause}
+	}
+}
+
+func handleOpErrors(f opErrorHandler, op offerResourceOp) offerResourceOp {
+	return func(operation Offer_Operation, resources Resources) (Resources, error) {
+		result, err := op(operation, resources)
+		if err != nil {
+			err = f(err)
+		}
+		return result, err
+	}
+}
 
 func validateOpTotals(f offerResourceOp) offerResourceOp {
 	return func(operation Offer_Operation, resources Resources) (Resources, error) {
@@ -38,21 +85,21 @@ func opReserve(operation Offer_Operation, resources Resources) (Resources, error
 	opRes := Resources(operation.GetReserve().GetResources())
 	err := opRes.Validate()
 	if err != nil {
-		return nil, fmt.Errorf("invalid RESERVE operation: %+v", err)
+		return nil, err
 	}
-	for _, r := range opRes {
-		if !r.IsReserved("") {
-			return nil, errors.New("invalid RESERVE operation: Resource must be reserved")
+	for i := range opRes {
+		if !opRes[i].IsReserved("") {
+			return nil, errors.New("Resource must be reserved")
 		}
-		if r.GetReservation() == nil {
-			return nil, errors.New("invalid RESERVE operation: missing 'reservation'")
+		if opRes[i].GetReservation() == nil {
+			return nil, errors.New("missing 'reservation'")
 		}
-		unreserved := Resources{r}.Flatten("", nil)
+		unreserved := Resources{opRes[i]}.Flatten("", nil)
 		if !result.ContainsAll(unreserved) {
-			return nil, fmt.Errorf("invalid RESERVE operation: %+v does not contain %+v", result, unreserved)
+			return nil, fmt.Errorf("%+v does not contain %+v", result, unreserved)
 		}
 		result.SubtractAll(unreserved)
-		result.Add(r)
+		result.Add(opRes[i])
 	}
 	return result, nil
 }
@@ -62,17 +109,17 @@ func opUnreserve(operation Offer_Operation, resources Resources) (Resources, err
 	opRes := Resources(operation.GetUnreserve().GetResources())
 	err := opRes.Validate()
 	if err != nil {
-		return nil, fmt.Errorf("invalid UNRESERVE operation: %+v", err)
+		return nil, err
 	}
-	for _, r := range opRes {
-		if !r.IsReserved("") {
-			return nil, errors.New("invalid UNRESERVE operation: Resource is not reserved")
+	for i := range opRes {
+		if !opRes[i].IsReserved("") {
+			return nil, errors.New("Resource is not reserved")
 		}
-		if r.GetReservation() == nil {
-			return nil, errors.New("invalid UNRESERVE operation: missing 'reservation'")
+		if opRes[i].GetReservation() == nil {
+			return nil, errors.New("missing 'reservation'")
 		}
-		unreserved := Resources{r}.Flatten("", nil)
-		result.Subtract(r)
+		unreserved := Resources{opRes[i]}.Flatten("", nil)
+		result.Subtract(opRes[i])
 		result.AddAll(unreserved)
 	}
 	return result, nil
@@ -83,14 +130,14 @@ func opCreate(operation Offer_Operation, resources Resources) (Resources, error)
 	volumes := Resources(operation.GetCreate().GetVolumes())
 	err := volumes.Validate()
 	if err != nil {
-		return nil, fmt.Errorf("invalid CREATE operation: %+v", err)
+		return nil, err
 	}
-	for _, v := range volumes {
-		if v.GetDisk() == nil {
-			return nil, errors.New("invalid CREATE operation: missing 'disk'")
+	for i := range volumes {
+		if volumes[i].GetDisk() == nil {
+			return nil, errors.New("missing 'disk'")
 		}
-		if v.GetDisk().GetPersistence() == nil {
-			return nil, errors.New("invalid CREATE operation: missing 'persistence'")
+		if volumes[i].GetDisk().GetPersistence() == nil {
+			return nil, errors.New("missing 'persistence'")
 		}
 		// from: https://github.com/apache/mesos/blob/master/src/common/resources.cpp
 		// Strip the disk info so that we can subtract it from the
@@ -99,13 +146,13 @@ func opCreate(operation Offer_Operation, resources Resources) (Resources, error)
 		// now. Persistent volumes can only be be created from regular
 		// disk resources. Revisit this once we start to support
 		// non-persistent volumes.
-		stripped := proto.Clone(&v).(*Resource)
+		stripped := proto.Clone(&volumes[i]).(*Resource)
 		stripped.Disk = nil
 		if !result.Contains(*stripped) {
 			return nil, errors.New("invalid CREATE operation: insufficient disk resources")
 		}
 		result.Subtract(*stripped)
-		result.Add(v)
+		result.Add(volumes[i])
 	}
 	return result, nil
 }
@@ -115,21 +162,21 @@ func opDestroy(operation Offer_Operation, resources Resources) (Resources, error
 	volumes := Resources(operation.GetDestroy().GetVolumes())
 	err := volumes.Validate()
 	if err != nil {
-		return nil, fmt.Errorf("invalid DESTROY operation: %+v", err)
+		return nil, err
 	}
-	for _, v := range volumes {
-		if v.GetDisk() == nil {
-			return nil, errors.New("invalid DESTROY operation: missing 'disk'")
+	for i := range volumes {
+		if volumes[i].GetDisk() == nil {
+			return nil, errors.New("missing 'disk'")
 		}
-		if v.GetDisk().GetPersistence() == nil {
-			return nil, errors.New("invalid DESTROY operation: missing 'persistence'")
+		if volumes[i].GetDisk().GetPersistence() == nil {
+			return nil, errors.New("missing 'persistence'")
 		}
-		if !result.Contains(v) {
-			return nil, errors.New("invalid DESTROY operation: persistent volume does not exist")
+		if !result.Contains(volumes[i]) {
+			return nil, errors.New("persistent volume does not exist")
 		}
-		stripped := proto.Clone(&v).(*Resource)
+		stripped := proto.Clone(&volumes[i]).(*Resource)
 		stripped.Disk = nil
-		result.Subtract(v)
+		result.Subtract(volumes[i])
 		result.Add(*stripped)
 	}
 	return result, nil
@@ -138,7 +185,10 @@ func opDestroy(operation Offer_Operation, resources Resources) (Resources, error
 func (operation Offer_Operation) Apply(resources Resources) (Resources, error) {
 	f, ok := offerResourceOpMap[operation.GetType()]
 	if !ok {
-		return nil, errors.New("unknown offer operation: " + operation.GetType().String())
+		return nil, &OperationError{
+			errorType: OperationErrorTypeUnknown,
+			cause:     errors.New("unknown offer operation: " + operation.GetType().String()),
+		}
 	}
 	return f(operation, resources)
 }

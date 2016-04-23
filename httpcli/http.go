95e37a120d46096da6421c9bb7ab889996d2f4b3
James DeFelice <james@mesosphere.io>
2016-04-23 01:22:06 +0000

    modularize error mapping

--- httpcli/http.go
+++ httpcli/http.go
@@ -10,7 +10,6 @@ import (
 	"net"
 	"net/http"
 	"net/url"
-	"strconv"
 	"time"
 
 	"github.com/mesos/mesos-go"
@@ -20,23 +19,25 @@ import (
 
 var (
 	// ErrNotLeader is returned by Do calls that are sent to a non leading Mesos master.
-	ErrNotLeader = errors.New("scheduler: call sent to a non-leading master")
+	ErrNotLeader = errors.New("mesos: call sent to a non-leading master")
 	// ErrAuth is returned by Do calls that are not successfully authenticated.
-	ErrAuth = errors.New("scheduler: call not authenticated")
+	ErrAuth = errors.New("mesos: call not authenticated")
 	// ErrUnsubscribed is returned by Do calls that are sent before a subscription is established.
-	ErrUnsubscribed = errors.New("scheduler: no subscription established")
+	ErrUnsubscribed = errors.New("mesos: no subscription established")
 	// ErrVersion is returned by Do calls that are sent to an incompatible API version.
-	ErrVersion = errors.New("scheduler: incompatible API version")
+	ErrVersion = errors.New("mesos: incompatible API version")
 	// ErrMalformed is returned by Do calls that are malformed.
-	ErrMalformed = errors.New("scheduler: malformed request")
+	ErrMalformed = errors.New("mesos: malformed request")
 	// ErrMediaType is returned by Do calls that are sent with an unsupported media type.
-	ErrMediaType = errors.New("scheduler: unsupported media type")
+	ErrMediaType = errors.New("mesos: unsupported media type")
 	// ErrRateLimit is returned by Do calls that are rate limited. This is a temporary condition
 	// that should clear.
-	ErrRateLimit = errors.New("scheduler: rate limited")
-	// ErrUnavailable is returned by Do calls that are sent to a master that's in recovery, or
+	ErrRateLimit = errors.New("mesos: rate limited")
+	// ErrUnavailable is returned by Do calls that are sent to a master or agent that's in recovery, or
 	// does not yet realize that it's the leader. This is a temporary condition that should clear.
-	ErrUnavailable = errors.New("scheduler: master unavailable")
+	ErrUnavailable = errors.New("mesos: mesos server unavailable")
+	// ErrNotFound could happen if the master or agent libprocess has not yet set up http routes
+	ErrNotFound = errors.New("mesos: mesos http endpoint not found")
 
 	// codeErrors maps HTTP response codes to their respective errors.
 	codeErrors = map[int]error{
@@ -48,10 +49,26 @@ var (
 		http.StatusForbidden:          ErrUnsubscribed,
 		http.StatusUnauthorized:       ErrAuth,
 		http.StatusNotAcceptable:      ErrMediaType,
+		http.StatusNotFound:           ErrNotFound,
 		http.StatusServiceUnavailable: ErrUnavailable,
 		http.StatusTooManyRequests:    ErrRateLimit,
 	}
+
+	defaultErrorMapper = ErrorMapperFunc(func(code int) error {
+		err, ok := codeErrors[code]
+		if !ok {
+			err = ProtocolError(code)
+		}
+		return err
+	})
 )
+
+// ProtocolError is a generic error type returned for expected status codes
+// received from Mesos.
+type ProtocolError int
+
+// Error implements error interface
+func (pe ProtocolError) Error() string { return fmt.Sprintf("Unexpected Mesos HTTP error: %d", int(pe)) }
 
 const (
 	debug                      = false
@@ -85,6 +102,9 @@ type Response struct {
 // implements mesos.Response
 func (r *Response) Decoder() encoding.Decoder { return r.decoder }
 
+// ErrorMapperFunc generates an error for the given statusCode
+type ErrorMapperFunc func(statusCode int) error
+
 // A Client is a Mesos HTTP APIs client.
 type Client struct {
 	url          string
@@ -92,6 +112,7 @@ type Client struct {
 	header       http.Header
 	codec        *encoding.Codec
 	maxRedirects int
+	errorMapper  ErrorMapperFunc
 }
 
 // New returns a new Client with the given Opts applied.
@@ -103,6 +124,7 @@ func New(opts ...Opt) *Client {
 		do:           With(),
 		header:       http.Header{},
 		maxRedirects: defaultMaxRedirectAttempts,
+		errorMapper:  defaultErrorMapper,
 	}
 	c.With(opts...)
 	return c
@@ -220,16 +242,21 @@ func (c *Client) Do(m encoding.Marshaler, opt ...RequestOpt) (mesos.Response, er
 				}
 			}
 		default:
-			if _, found := codeErrors[res.StatusCode]; !found {
-				// TODO(jdef) this is terrible, but handy during development; we should probably just
-				// return a ProtocolError or something
-				panic("unexpected status code " + strconv.Itoa(int(res.StatusCode)))
-			}
+			err = c.errorMapper(res.StatusCode)
 		}
 		return &Response{
 			decoder: events,
 			Closer:  res.Body,
-		}, codeErrors[res.StatusCode]
+		}, err
+	}
+}
+
+// ErrorMapper returns am Opt that overrides the existing error mapping behavior of the client.
+func ErrorMapper(em ErrorMapperFunc) Opt {
+	return func(c *Client) Opt {
+		old := c.errorMapper
+		c.errorMapper = em
+		return ErrorMapper(old)
 	}
 }
 

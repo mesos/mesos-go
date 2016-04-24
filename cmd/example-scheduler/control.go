8a5b4412e288b89d2ee9efa6c04a62850265c63e
James DeFelice <james@mesosphere.io>
2016-04-24 03:05:57 +0000

    simplify main, refactor metrics API

---
+++ cmd/example-scheduler/control.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"time"
+)
+
+func forever(name string, jobRestartDelay time.Duration, counter metricCounter, f func() error) {
+	for {
+		counter(name)
+		err := f()
+		if err != nil {
+			log.Printf("job %q exited with error %+v", name, err)
+		} else {
+			log.Printf("job %q exited", name)
+		}
+		time.Sleep(jobRestartDelay)
+	}
+}

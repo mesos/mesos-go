330d473ae9d5d16d2326f16ee4473b66a03ccc98
James DeFelice <james@mesosphere.io>
2016-04-24 03:14:02 +0000

    add functional variant of Client

--- client.go
+++ client.go
@@ -13,6 +13,12 @@ type Client interface {
 	Do(encoding.Marshaler) (*Response, error)
 }
 
+// ClientFunc is a functional variant (and implementation) of the Client interface
+type ClientFunc func(encoding.Marshaler) (*Response, error)
+
+// Do implements Client
+func (cf ClientFunc) Do(m encoding.Marshaler) (*Response, error) { return cf(m) }
+
 // Response captures the output of a Mesos API operation. Callers are responsible for invoking
 // Close when they're finished processing the response otherwise there may be connection leaks.
 type Response interface {

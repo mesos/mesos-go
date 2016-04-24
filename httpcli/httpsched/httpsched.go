3554a6a0fb27e6fcf6d90613d9cc0728a4a644ee
James DeFelice <james@mesosphere.io>
2016-04-24 15:24:43 +0000

    refactor redirect logic into httpsched package

--- httpcli/httpsched/httpsched.go
+++ httpcli/httpsched/httpsched.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"log"
 	"net/http"
+	"net/url"
 
 	"github.com/mesos/mesos-go"
 	"github.com/mesos/mesos-go/encoding"
@@ -19,7 +20,9 @@ var errMissingMesosStreamId = errors.New("missing Mesos-Stream-Id header expecte
 
 // CallNoData is for scheduler calls that are not expected to return any data from the server.
 func CallNoData(cli *httpcli.Client, call encoding.Marshaler) error {
-	resp, err := cli.Do(call)
+	resp, err := callWithRedirect(cli, func() (mesos.Response, error) {
+		return cli.Do(call)
+	})
 	if resp != nil {
 		resp.Close()
 	}
@@ -59,9 +62,52 @@ func Subscribe(cli *httpcli.Client, subscribe encoding.Marshaler) (resp mesos.Re
 		})
 	)
 	cli.WithTemporary(opt, func() error {
-		resp, subscribeErr = cli.Do(subscribe, httpcli.Close(true))
+		resp, subscribeErr = callWithRedirect(cli, func() (mesos.Response, error) {
+			return cli.Do(subscribe, httpcli.Close(true))
+		})
 		return nil
 	})
 	maybeOpt = httpcli.DefaultHeader(headerMesosStreamID, mesosStreamID)
 	return
+}
+
+func callWithRedirect(cli *httpcli.Client, f func() (mesos.Response, error)) (resp mesos.Response, err error) {
+	var (
+		attempt      = 0
+		maxRedirects = cli.MaxRedirects()
+	)
+	for {
+		resp, err = f()
+		if err == nil || (err != nil && err != httpcli.ErrNotLeader) {
+			return resp, err
+		}
+
+		res := resp.(*httpcli.Response)
+
+		// TODO(jdef) refactor this
+		// mesos v0.29 will actually send back fully-formed URLs in the Location header
+		log.Println("master changed?")
+		if attempt < maxRedirects {
+			attempt++
+			newMaster := res.Header.Get("Location")
+			if newMaster != "" {
+				// current format appears to be //x.y.z.w:port
+				hostport, parseErr := url.Parse(newMaster)
+				if parseErr != nil || hostport.Host == "" {
+					return
+				}
+				current, parseErr := url.Parse(cli.URL())
+				if parseErr != nil {
+					return
+				}
+				current.Host = hostport.Host
+				endpoint := current.String()
+				log.Println("redirecting to " + endpoint)
+				cli.With(httpcli.URL(endpoint))
+				res.Close()
+				continue
+			}
+		}
+		return
+	}
 }

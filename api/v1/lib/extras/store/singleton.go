918cdfebc84f4a8425c700f7e1bcba4d1cd6c85d
James DeFelice <james@mesosphere.io>
2017-05-25 17:38:38 +0000

    overhaul existing examples and scheduler controller infra

--- api/v1/lib/extras/store/singleton.go
+++ api/v1/lib/extras/store/singleton.go
@@ -1,13 +1,16 @@
 package store
 
-import "sync/atomic"
+import (
+	"errors"
+	"sync/atomic"
+)
 
 type (
 	Getter interface {
-		Get() string
+		Get() (string, error)
 	}
 
-	GetFunc func() string
+	GetFunc func() (string, error)
 
 	Setter interface {
 		Set(string) error
@@ -25,24 +28,130 @@ type (
 		GetFunc
 		SetFunc
 	}
+
+	SingletonDecorator interface {
+		Decorate(Singleton) Singleton
+	}
 )
 
-func (f GetFunc) Get() string        { return f() }
-func (f SetFunc) Set(s string) error { return f(s) }
+func (f GetFunc) Get() (string, error) { return f() }
+func (f SetFunc) Set(s string) error   { return f(s) }
+
+var ErrNotFound = errors.New("value not found in store")
 
 func NewInMemorySingleton() Singleton {
 	var value atomic.Value
 	return &SingletonAdapter{
-		func() string {
+		func() (string, error) {
 			x := value.Load()
 			if x == nil {
-				return ""
+				return "", ErrNotFound
 			}
-			return x.(string)
+			return x.(string), nil
 		},
 		func(s string) error {
 			value.Store(s)
 			return nil
 		},
+	}
+}
+
+type (
+	GetFuncDecorator func(Getter, string, error) (string, error)
+	SetFuncDecorator func(Setter, string, error) error
+)
+
+func DoSet() SetFuncDecorator {
+	return func(s Setter, v string, _ error) error {
+		return s.Set(v)
+	}
+}
+
+func (f SetFuncDecorator) AndThen(f2 SetFuncDecorator) SetFuncDecorator {
+	return func(s Setter, v string, err error) error {
+		err = f(s, v, err)
+		if err != nil {
+			return err
+		}
+		return f2(s, v, nil)
+	}
+}
+
+func (f SetFuncDecorator) Decorate(s Singleton) Singleton {
+	if f == nil {
+		return s
+	}
+	return &SingletonAdapter{
+		s.Get,
+		SetFunc(func(v string) error {
+			return f(s, v, nil)
+		}),
+	}
+}
+
+func DoGet() GetFuncDecorator {
+	return func(s Getter, _ string, _ error) (string, error) {
+		return s.Get()
+	}
+}
+
+func (f GetFuncDecorator) AndThen(f2 GetFuncDecorator) GetFuncDecorator {
+	return func(s Getter, v string, err error) (string, error) {
+		v, err = f(s, v, err)
+		if err != nil {
+			return v, err
+		}
+		return f2(s, v, nil)
+	}
+}
+
+func (f GetFuncDecorator) Decorate(s Singleton) Singleton {
+	if f == nil {
+		return s
+	}
+	return &SingletonAdapter{
+		GetFunc(func() (string, error) {
+			return f(s, "", nil)
+		}),
+		s.Set,
+	}
+}
+
+func DecorateSingleton(s Singleton, ds ...SingletonDecorator) Singleton {
+	for _, d := range ds {
+		if d != nil {
+			s = d.Decorate(s)
+		}
+	}
+	return s
+}
+
+// GetOrPanic curries the result of a Getter invocation: the returned func only ever returns the string component when
+// the error component of the underlying Get() call is nil. If Get() generates an error then the curried func panics.
+func GetOrPanic(g Getter) func() string {
+	return func() string {
+		v, err := g.Get()
+		if err != nil {
+			panic(err)
+		}
+		return v
+	}
+}
+
+func GetIgnoreErrors(g Getter) func() string {
+	return func() string {
+		v, _ := g.Get()
+		return v
+	}
+}
+
+// SetOrPanic curries the result of a Setter invocation: the returned func only ever returns normally when the error
+// component of the underlying Set() call is nil. If Set() generates an error then the curried func panics.
+func SetOrPanic(s Setter) func(v string) {
+	return func(v string) {
+		err := s.Set(v)
+		if err != nil {
+			panic(err)
+		}
 	}
 }

ae8a86ffc46604bd6c589a22cd95f52782efada9
James DeFelice <james@mesosphere.io>
2017-11-08 13:16:06 +0000

    extras: generic scheduler controller ack rule for offer operation status updates

--- api/v1/lib/scheduler/calls/errors.go
+++ api/v1/lib/scheduler/calls/errors.go
@@ -5,9 +5,11 @@ import (
 )
 
 // AckError wraps a caller-generated error and tracks the call that failed.
+// It may be reported for either a task status ACK error, or an offer operation
+// status ACK error.
 type AckError struct {
-	Ack   *scheduler.Call
-	Cause error
+	Ack   *scheduler.Call // Ack is REQUIRED
+	Cause error           // Cause is REQUIRED
 }
 
 func (err *AckError) Error() string { return err.Cause.Error() }
